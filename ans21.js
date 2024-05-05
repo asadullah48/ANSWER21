3efb2c17758c82ffa716f10e49086dc8cc70925c
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-05 16:19:29 +0500

    Update ans21.js
    edited

--- ans21.js
+++ ans21.js
@@ -1,9 +1,25 @@
 //Think of something you could store in a TypeScript Object.
 //Write a program that creates Objects containing these items.
-var book = {
+let book = {
     title: "Arabic Course",
     author: "Dr. V. Abdur Rahim",
     publisher: "Darussalam",
     yearPublished: 1428,
 };
-console.log("Book Info: ".concat(book.title, " by ").concat(book.author, ", published by ").concat(book.publisher, ", published in ").concat(book.yearPublished));
+console.log(`Book Info: ${book.title} by ${book.author}, published by ${book.publisher}, published in ${book.yearPublished}`);
+import inquirer from "inquirer";
+const result = await inquirer.prompt([
+    { message: "fristNumber", type: "number", name: "fristNumber" },
+    { message: "secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multipication", "Division", "Percentage"],
+    },
+]);
+if (result.operator === "Addition") {
+    let results = result.fristNumber + result.secondNumber;
+    console.log(results);
+}
+;
