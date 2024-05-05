2fedee83588a33208ab7089e41d8336694581ac2
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-05 16:06:40 +0500

    Initial commit

---
+++ ans21.js
@@ -0,0 +1,9 @@
+//Think of something you could store in a TypeScript Object.
+//Write a program that creates Objects containing these items.
+var book = {
+    title: "Arabic Course",
+    author: "Dr. V. Abdur Rahim",
+    publisher: "Darussalam",
+    yearPublished: 1428,
+};
+console.log("Book Info: ".concat(book.title, " by ").concat(book.author, ", published by ").concat(book.publisher, ", published in ").concat(book.yearPublished));
